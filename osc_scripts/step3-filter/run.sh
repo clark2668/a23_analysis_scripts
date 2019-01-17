a2aae69109ec32c09d7bc9444abf2e1ed593677a
Brian Clark <clark.2668@osu.edu>
2019-01-17 11:49:46 -0500

    Move logging file location and other minor tweaks

--- osc_scripts/step3-filter/run.sh
+++ osc_scripts/step3-filter/run.sh
@@ -2,11 +2,11 @@
 #PBS -l nodes=1:ppn=1
 #PBS -l mem=4GB
 #PBS -l walltime=01:00:00
-#PBS -A PAS0654
+#PBS -A PCON0003
 #PBS -e /fs/scratch/PAS0654/ara/10pct/err_out_logs
 #PBS -o /fs/scratch/PAS0654/ara/10pct/err_out_logs
 
-eval 'source /users/PAS0654/osu0673/A23_analysis/env.sh'
+eval 'source /users/PAS0654/osu0673/A23_analysis_new2/env.sh'
 
 cd $RUNDIR
 
@@ -14,7 +14,7 @@ cd $RUNDIR
 
 if [ $? -ne 0 ] #error handle
 then
-	echo $FILE $PED >> /fs/scratch/PAS0654/ara/10pct/ProcessedFile/problems_A${STATION}_${YEAR}_rd1.txt
+	echo $FILE $PED >> $ERRFILE
 else
 	pbsdcp $TMPDIR/'*.root' $OUTPUTDIR
 fi
--- osc_scripts/step3-filter/submit_jobs.sh
+++ osc_scripts/step3-filter/submit_jobs.sh
@@ -4,7 +4,7 @@ station="3"
 echo '[ Station: ' $station ' ]'
 export station
 
-year="2013"
+year="2015"
 echo '[ Station: ' $year ' ]'
 export year
 
@@ -13,20 +13,24 @@ echo '[ Simulation: ' $simulation ' ]'
 export simulation
 
 #where should the outputs be stored?
-OutputDir="/fs/scratch/PAS0654/ara/10pct/ProcessedFile/A${station}/${year}"
+OutputDir="/fs/scratch/PAS0654/ara/10pct/CompareFilter/A${station}/${year}"
 echo '[ Processed file output directory: ' $OutputDir ' ]'
 export OutputDir
+
+#what error file should we write to?
+ErrFile="/fs/scratch/PAS0654/ara/10pct/CompareFilter/problems_A${station}_${year}.txt"
+echo '[ Run summary directory: ' $SummaryDir ' ]'
+export SummaryDir
 
 #where are the run summaries?
 SummaryDir="/fs/scratch/PAS0654/ara/10pct/RunSummary/A${station}/${year}"
 echo '[ Run summary directory: ' $SummaryDir ' ]'
 export SummaryDir
 
-RunDir="/users/PAS0654/osu0673/A23_analysis/araROOT"
+RunDir="/users/PAS0654/osu0673/A23_analysis_new2/araROOT"
 export RunDir
 
 readfile=../step1-make_ped_pairs/A${station}_${year}_File_Ped_Pairs.txt
-readfile=reque.txt
 counter=0
 while read line1
 do
@@ -37,7 +41,7 @@ do
 
 	echo $line1
 
-	qsub -v RUNDIR=$RunDir,OUTPUTDIR=$OutputDir,SUMMARYDIR=$SummaryDir,STATION=$station,YEAR=$year,SIMULATION=$simulation,FILE=$f1,PED=$p1 -N 'A'$station'_'$year'_'$counter run.sh
+	qsub -v ERRFILE=$ErrFilek,RUNDIR=$RunDir,OUTPUTDIR=$OutputDir,SUMMARYDIR=$SummaryDir,STATION=$station,YEAR=$year,SIMULATION=$simulation,FILE=$f1,PED=$p1 -N 'A'$station'_'$year'_'$counter run.sh
 	counter=$((counter+1))
 
 done < $readfile
