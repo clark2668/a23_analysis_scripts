12dc6ea9ebb0e0cdf0f9fa805fc62ad8a9617923
Brian Clark <clark.2668@osu.edu>
2019-01-31 23:42:28 -0500

    Update monoE script again

--- osc_scripts/run_arasim/run_monoE.sh
+++ osc_scripts/run_arasim/run_monoE.sh
@@ -5,7 +5,7 @@
 #PBS -e /fs/scratch/PAS0654/ara/sim/err_out_logs
 #PBS -o /fs/scratch/PAS0654/ara/sim/err_out_logs
 
-eval 'source /users/PAS0654/osu0673/A23_analysis/env.sh'
+eval 'source /users/PAS0654/osu0673/A23_analysis_new2/env.sh'
 
 cd $RUN_DIR
 
@@ -37,9 +37,9 @@ done
 wait
 pbsdcp $TMPDIR/'*.root' $OUTPUT_DIR
 
-# for i in {161..200}
-# do
-# 	./AraSim $INPUTFILE $(($i+$OFFSET)) $TMPDIR & 
-# done
-# wait
-# pbsdcp $TMPDIR/'*.root' $OUTPUT_DIR
+for i in {161..200}
+do
+	./AraSim $INPUTFILE $(($i+$OFFSET)) $TMPDIR & 
+done
+wait
+pbsdcp $TMPDIR/'*.root' $OUTPUT_DIR
