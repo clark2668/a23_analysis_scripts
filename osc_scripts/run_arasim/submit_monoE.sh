5ccc6cdc54d4027c48110fc4ccff84af5b5375f4
Brian Clark <brianclark@Brians-MacBook-Pro.local>
2019-01-31 23:23:17 -0500

    Add some araSIM scripts with config files and submission scripts

---
+++ osc_scripts/run_arasim/submit_monoE.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+declare -a en=("16" "17" "18" "19" "20")
+
+for j in "${en[@]}"
+do
+
+	offset=0
+	export offset
+
+	AraSimDir='/users/PAS0654/osu0673/A23_analysis/arasim/trunk'
+	SetUpFile='/users/PAS0654/osu0673/A23_analysis/arasim/trunk/SETUP/ara2_E'$j'.txt'
+	OutputDir='/fs/scratch/PAS0654/ara_source/sim/sky_coverage/RawSim/E'$j
+
+	export AraSimDir
+	export SetUpFile
+	export OutputDir
+
+	echo ""
+	echo "--------------------------------------------"
+	echo "----- Preparing to batch submit AraSim "
+	echo "----- "
+	echo "----- AraSimDir: " $AraSimDir
+	echo "----- SetUpFile: " $SetUpFile
+	echo "----- outputDir: " $OutputDir
+	echo "----- Offset: " $offset
+	echo "----- "
+	echo "--------------------------------------------"
+	echo ""
+
+	qsub  -v INPUTFILE=$SetUpFile,RUN_DIR=$AraSimDir,OUTPUT_DIR=$OutputDir,OFFSET=$offset -N 'arasim_E'$j'_set'$offset run_monoE.sh
+
+	if [ "$j" -eq "16" -o "$j" -eq "165" ]
+	then
+		offset=$((offset+200))
+		qsub  -v INPUTFILE=$SetUpFile,RUN_DIR=$AraSimDir,OUTPUT_DIR=$OutputDir,OFFSET=$offset -N 'arasim_E'$j'_set'$offset run_monoE.sh
+	fi
+done
