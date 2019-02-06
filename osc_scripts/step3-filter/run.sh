57c0ea5187bf5a8020733e3d6506c812a9589260
clark2668 <clark.2668@osu.edu>
2019-02-06 13:00:07 -0500

    update run script to be more intelligent when choosing between sim and data

--- osc_scripts/step3-filter/run.sh
+++ osc_scripts/step3-filter/run.sh
@@ -1,19 +1,20 @@
 #/bin/bash
 #PBS -l nodes=1:ppn=1
 #PBS -l mem=4GB
-#PBS -l walltime=01:00:00
+#PBS -l walltime=02:00:00
 #PBS -A PCON0003
-#PBS -e /fs/scratch/PAS0654/ara/sim/err_out_logs
-#PBS -o /fs/scratch/PAS0654/ara/sim/err_out_logs
+#PBS -e /fs/scratch/PAS0654/ara/10pct/err_out_logs
+#PBS -o /fs/scratch/PAS0654/ara/10pct/err_out_logs
 
 #/fs/scratch/PAS0654/ara/10pct/err_out_logs
-#/fs/scratch/PAS0654/ara/10pct/err_out_logs
+#/fs/scratch/PAS0654/ara/sim/err_out_logs
 
 eval 'source /users/PAS0654/osu0673/A23_analysis_new2/env.sh'
 
 cd $RUNDIR
 
-./bin/CompareFilters $SIMULATION $STATION $SUMMARYDIR $TMPDIR $FILE $PED
+./bin/v2_Filter $SIMULATION $STATION $SUMMARYDIR $TMPDIR $FILE $PED
+#./bin/CompareFilters $SIMULATION $STATION $SUMMARYDIR $TMPDIR $FILE $PED
 
 if [ $? -ne 0 ] #error handle
 then
