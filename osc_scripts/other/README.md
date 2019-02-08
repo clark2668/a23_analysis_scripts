606ed118e2326a698b198f6630204259df2dc366
clark2668 <clark.2668@osu.edu>
2019-02-08 10:17:23 -0500

    Add scripts to execute other sub studies

---
+++ osc_scripts/other/README.md
@@ -0,0 +1,3 @@
+A smattering of other scripts that I use to execute different studies.
+
+These likely don't "drive" the central analysis in the way the other scripts do.
---
+++ osc_scripts/other/compare_filters/run.sh
@@ -0,0 +1,23 @@
+#/bin/bash
+#PBS -l nodes=1:ppn=1
+#PBS -l mem=4GB
+#PBS -l walltime=02:00:00
+#PBS -A PAS0654
+#PBS -e /fs/scratch/PAS0654/ara/sim/err_out_logs
+#PBS -o /fs/scratch/PAS0654/ara/sim/err_out_logs
+
+#/fs/scratch/PAS0654/ara/10pct/err_out_logs
+#/fs/scratch/PAS0654/ara/sim/err_out_logs
+
+eval 'source /users/PAS0654/osu0673/A23_analysis_new2/env.sh'
+
+cd $RUNDIR
+
+./bin/CompareFilters $SIMULATION $STATION $SUMMARYDIR $TMPDIR $FILE $PED
+
+if [ $? -ne 0 ] #error handle
+then
+	echo $FILE $PED >> $ERRFILE
+else
+	pbsdcp $TMPDIR/'*.root' $OUTPUTDIR
+fi
