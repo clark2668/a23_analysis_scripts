0519aa7b20a5e2d9e31892dae364850cd317377c
clark2668 <clark.2668@osu.edu>
2019-02-18 11:28:32 -0500

    wrong err log file output location

--- osc_scripts/run_diffuse/step6-save_vals_for_cuts/run.sh
+++ osc_scripts/run_diffuse/step6-save_vals_for_cuts/run.sh
@@ -2,9 +2,9 @@
 #PBS -l nodes=1:ppn=1
 #PBS -l mem=4GB
 #PBS -l walltime=2:30:00
-#PBS -A PAS0654
-#PBS -e /fs/scratch/PAS0654/ara/10pct/err_out_logs/save_vals_v2
-#PBS -o /fs/scratch/PAS0654/ara/10pct/err_out_logs/save_vals_v2
+#PBS -A PCON0003
+#PBS -e /fs/scratch/PAS0654/ara/10pct/err_out_logs
+#PBS -o /fs/scratch/PAS0654/ara/10pct/err_out_logs
 
 eval 'source /users/PAS0654/osu0673/A23_analysis/env.sh'
 
