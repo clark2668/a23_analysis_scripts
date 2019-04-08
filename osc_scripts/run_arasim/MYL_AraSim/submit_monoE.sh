c4202394a054c3ec68df2e50de90265197a9b389
clark2668 <clark.2668@osu.edu>
2019-04-08 09:45:26 -0400

    Chane naming on energies from 18 to 18.0

--- osc_scripts/run_arasim/MYL_AraSim/submit_monoE.sh
+++ osc_scripts/run_arasim/MYL_AraSim/submit_monoE.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-
-# declare -a en=("16" "17" "18" "19" "20")
-declare -a en=("17" "18" "19" "20")
-# declare -a en=("20")
-# declare -a con=("1" "2" "3" "4" "5")
+# declare -a en=("20.0" "19.0" "18.0" "17.0")
+# declare -a en=("17.0" "18.0" "19.0" "20.0")
+declare -a en=("17.0")
 declare -a con=("4")
-
 station="2"
 
 for config in "${con[@]}"
@@ -13,12 +10,14 @@ do
 	for energy in "${en[@]}"
 	do
 
-		offset=0
+		offset=400
 		export offset
 
 		AraSimDir='/users/PAS0654/osu0673/A23_analysis_new2/AraSim_versions/ARA0'$station'_type_'$config
 		SetUpFile='/users/PAS0654/osu0673/A23_analysis_new2/a23_analysis_scripts/osc_scripts/run_arasim/MYL_AraSim/setups/A'$station'_c'$config'_E'$energy'.txt'
+		# SetUpFile='/users/PAS0654/osu0673/A23_analysis_new2/a23_analysis_scripts/osc_scripts/run_arasim/MYL_AraSim/setups/A'$station'_c'$config'_E'$energy'_noise.txt'
 		OutputDir='/fs/scratch/PAS0654/ara/sim/RawSim/A'$station'/c'$config'/E'$energy
+		# OutputDir='/fs/scratch/PAS0654/ara/sim/RawSim/noise_only'
 
 		echo $AraSimDir
 		echo $SetUpFile
@@ -37,17 +36,15 @@ do
 		echo ""
 
 		qsub  -v INPUTFILE=$SetUpFile,RUN_DIR=$AraSimDir,OUTPUT_DIR=$OutputDir,OFFSET=$offset -N 'arasim_A'$station'_E'$energy'_config'$config'_set'$offset run_monoE.sh
+		offset=$((offset+200))
+		qsub  -v INPUTFILE=$SetUpFile,RUN_DIR=$AraSimDir,OUTPUT_DIR=$OutputDir,OFFSET=$offset -N 'arasim_A'$station'_E'$energy'_config'$config'_set'$offset run_monoE.sh
 
-		# if [ "$j" -eq "18" -o "$j" -eq "165" ]
+		# if [ "$energy" -eq "17.0" -o "$energy" -eq "17.5" ]
 		# then
-		# 	offset=$((offset+80))
-		# 	qsub  -v INPUTFILE=$SetUpFile,RUN_DIR=$AraSimDir,OUTPUT_DIR=$OutputDir,OFFSET=$offset -N 'arasim_A'$station'_E'$j'_set'$offset run_monoE.sh
-		# 	offset=$((offset+80))
-		# 	qsub  -v INPUTFILE=$SetUpFile,RUN_DIR=$AraSimDir,OUTPUT_DIR=$OutputDir,OFFSET=$offset -N 'arasim_A'$station'_E'$j'_set'$offset run_monoE.sh
-		# 	offset=$((offset+80))
-		# 	qsub  -v INPUTFILE=$SetUpFile,RUN_DIR=$AraSimDir,OUTPUT_DIR=$OutputDir,OFFSET=$offset -N 'arasim_A'$station'_E'$j'_set'$offset run_monoE.sh
-		# 	offset=$((offset+80))
-		# 	qsub  -v INPUTFILE=$SetUpFile,RUN_DIR=$AraSimDir,OUTPUT_DIR=$OutputDir,OFFSET=$offset -N 'arasim_A'$station'_E'$j'_set'$offset run_monoE.sh
+		# 	offset=$((offset+200))
+		# 	qsub  -v INPUTFILE=$SetUpFile,RUN_DIR=$AraSimDir,OUTPUT_DIR=$OutputDir,OFFSET=$offset -N 'arasim_A'$station'_E'$energy'_config'$config'_set'$offset run_monoE.sh
+		# 	offset=$((offset+200))
+		# 	qsub  -v INPUTFILE=$SetUpFile,RUN_DIR=$AraSimDir,OUTPUT_DIR=$OutputDir,OFFSET=$offset -N 'arasim_A'$station'_E'$energy'_config'$config'_set'$offset run_monoE.sh
 		# fi
 	done
 done
