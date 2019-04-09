475137196456c68959e17858f362f100b741631e
clark2668 <clark.2668@osu.edu>
2019-04-09 10:57:26 -0400

    remove year arguments, which are unnecessary in the configuration framework

--- osc_scripts/run_diffuse/step6-save_vals_for_cuts/v2_multi/pitzer_submit_jobs.sh
+++ osc_scripts/run_diffuse/step6-save_vals_for_cuts/v2_multi/pitzer_submit_jobs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-station="2"
+station="3"
 echo '[ Station: ' $station ' ]'
 export station
 
@@ -8,15 +8,11 @@ simulation='0'
 echo '[ Simulation: ' $simulation ' ]'
 export simulation
 
-year="2015"
-echo '[ Station: ' $year ' ]'
-export year
-
 energy="18.0"
 echo '[ Energy: ' $energy ']'
 export energy
 
-config="4"
+config="1"
 echo '[ Config: ' $config ']'
 export config
 
@@ -29,11 +25,11 @@ then
 	OutputDir="/fs/scratch/PAS0654/ara/sim/ValsForCuts/A${station}/c${config}/E${energy}"
 elif [ $simulation == '0' ] #is not simulation
 then
-	OutputDir="/fs/scratch/PAS0654/ara/10pct/ValsForCuts/A${station}/${year}/test_small"
+	OutputDir="/fs/scratch/PAS0654/ara/10pct/ValsForCuts/A${station}/by_config/c${config}"
 fi
 
 #where should the outputs be stored?
-echo '[ Processed file output directory: ' $OutputDir ' ]'
+echo '[ Output directory: ' $OutputDir ' ]'
 export OutputDir
 
 RunDir="/users/PAS0654/osu0673/A23_analysis_new2/AraRoot"
@@ -46,9 +42,10 @@ export LaunchDir
 
 #we have to define where in the list of data files we want to start
 FileNumberStart=0
-FileNumberEnd=40
+FileNumberEnd=5000
 
-readfile=../A${station}_${year}_MergedFiles_pitzer.txt
+# readfile=../A${station}_${year}_MergedFiles_pitzer.txt
+readfile=../data_by_config/A${station}_c${config}_MergedFiles_pitzer.txt
 
 FileNumber=0
 while read line1 && read line2 && read line3 && read line4 && read line5 && read line6 && read line7 && read line8 && read line9 && read line10 && read line11 && read line12 && read line13 && read line14 && read line15 && read line16 && read line17 && read line18 && read line19 && read line20 && read line21 && read line22 && read line23 && read line24 && read line25 && read line26 && read line27 && read line28 && read line29 && read line30 && read line31 && read line32 && read line33 && read line34 && read line35 && read line36 && read line37 && read line38 && read line39 && read line40
@@ -180,7 +177,7 @@ do
 		f40=${sa40[0]}
 
 
-		qsub -v LAUNCHDIR=$LaunchDir,RUNDIR=$RunDir,OUTPUTDIR=$OutputDir,DROPCHANS=$DropBadChans,STATION=$station,YEAR=$year,CONFIG=$config,ENERGY=$energy,SIMULATION=$simulation,F1=$f1,P1=$p1,F2=$f2,P2=$p2,F3=$f3,P3=$p3,F4=$f4,P4=$p4,F5=$f5,P5=$p5,F6=$f6,P6=$p6,F7=$f7,P7=$p7,F8=$f8,P8=$p8,F9=$f9,P9=$p9,F10=$f10,P10=$p10,F11=$f11,P11=$p11,F12=$f12,P12=$p12,F13=$f13,P13=$p13,F14=$f14,P14=$p14,F15=$f15,P15=$p15,F16=$f16,P16=$p16,F17=$f17,P17=$p17,F18=$f18,P18=$p18,F19=$f19,P19=$p19,F20=$f20,P20=$p20,F21=$f21,P21=$p21,F22=$f22,P22=$p22,F23=$f23,P23=$p23,F24=$f24,P24=$p24,F25=$f25,P25=$p25,F26=$f26,P26=$p26,F27=$f27,P27=$p27,F28=$f28,P28=$p28,F29=$f29,P29=$p29,F30=$f30,P30=$p30,F31=$f31,P31=$p31,F32=$f32,P32=$p32,F33=$f33,P33=$p33,F34=$f34,P34=$p34,F35=$f35,P35=$p35,F36=$f36,P36=$p36,F37=$f37,P37=$p37,F38=$f38,P38=$p38,F39=$f39,P39=$p39,F40=$f40,P40=$f40 -N 'pitzer_multi40_A'$station'_'$year'_'$config'_'$energy'_'$FileNumber'_savevals' pitzer_run.sh
+		qsub -v LAUNCHDIR=$LaunchDir,RUNDIR=$RunDir,OUTPUTDIR=$OutputDir,DROPCHANS=$DropBadChans,STATION=$station,CONFIG=$config,ENERGY=$energy,SIMULATION=$simulation,F1=$f1,P1=$p1,F2=$f2,P2=$p2,F3=$f3,P3=$p3,F4=$f4,P4=$p4,F5=$f5,P5=$p5,F6=$f6,P6=$p6,F7=$f7,P7=$p7,F8=$f8,P8=$p8,F9=$f9,P9=$p9,F10=$f10,P10=$p10,F11=$f11,P11=$p11,F12=$f12,P12=$p12,F13=$f13,P13=$p13,F14=$f14,P14=$p14,F15=$f15,P15=$p15,F16=$f16,P16=$p16,F17=$f17,P17=$p17,F18=$f18,P18=$p18,F19=$f19,P19=$p19,F20=$f20,P20=$p20,F21=$f21,P21=$p21,F22=$f22,P22=$p22,F23=$f23,P23=$p23,F24=$f24,P24=$p24,F25=$f25,P25=$p25,F26=$f26,P26=$p26,F27=$f27,P27=$p27,F28=$f28,P28=$p28,F29=$f29,P29=$p29,F30=$f30,P30=$p30,F31=$f31,P31=$p31,F32=$f32,P32=$p32,F33=$f33,P33=$p33,F34=$f34,P34=$p34,F35=$f35,P35=$p35,F36=$f36,P36=$p36,F37=$f37,P37=$p37,F38=$f38,P38=$p38,F39=$f39,P39=$p39,F40=$f40,P40=$f40 -N 'pitzer_multi40_A'$station'_'$year'_'$config'_'$energy'_'$FileNumber'_savevals' pitzer_run.sh
 		echo "-----------------------------------"
 			
 	fi
