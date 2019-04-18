e456b4e8aab1a5db06cdd28d4b8157cca816c9cc
clark2668 <clark.2668@osu.edu>
2019-04-18 11:47:07 -0400

    upgrade join code to be friendlier to simulation

--- osc_scripts/run_diffuse/step5-join_filter_reco/submit_jobs.sh
+++ osc_scripts/run_diffuse/step5-join_filter_reco/submit_jobs.sh
@@ -1,33 +1,63 @@
 #!/bin/bash
 
-station="3"
+station="2"
 echo '[ Station: ' $station ' ]'
 export station
 
-year="2015"
+year="2013"
 echo '[ Station: ' $year ' ]'
 export year
 
+simulation='1'
+echo '[ Simulation: ' $simulation ' ]'
+export simulation
+
+energy='18.0'
+echo '[ Energy: ' $energy ']'
+export energy
+
+config="4"
+echo '[ Config: ' $config ']'
+export config
+
+if [ $simulation == '1' ] #is simulation
+then
+	OutputDir="/fs/scratch/PAS0654/ara/sim/Joined/A${station}/c${config}/E${energy}"
+	ProcDir="/fs/scratch/PAS0654/ara/sim/ProcessedFile/A${station}/c${config}/E${energy}"
+	ErrFile="/fs/scratch/PAS0654/ara/sim/Joined/sim_joinproblems_A${station}_E${energy}.txt"
+	readfile=A${station}_c${config}_E{energy}_FilterList.txt
+	err_out_location=/fs/scratch/PAS0654/ara/sim/err_out_logs
+
+elif [ $simulation == '0' ] #is not simulation
+then
+	OutputDir="/fs/scratch/PAS0654/ara/10pct/Joined/A${station}/${year}"
+	ProcDir="/fs/scratch/PAS0654/ara/10pct/ProcessedFile/A${station}/${year}"
+	ErrFile="/fs/scratch/PAS0654/ara/10pct/Joined/data_joinproblems_A${station}_${year}.txt"
+	readfile=A${station}FilterList_1in10_${year}.txt
+	err_out_location=/fs/scratch/PAS0654/ara/10pct/err_out_logs
+fi
+
 #where should the outputs be stored?
-OutputDir="/fs/scratch/PAS0654/ara/10pct/Joined/A${station}/${year}"
 echo '[ Output directory: ' $OutputDir ' ]'
 export OutputDir
 
-ProcDir="/fs/scratch/PAS0654/ara/10pct/ProcessedFile/A${station}/${year}"
 echo '[ Proc file directory: ' $ProcDir ' ]'
 export ProcDir
 
 RunDir="/users/PAS0654/osu0673/A23_analysis_new2/AraRoot"
 export RunDir
 
-readfile=A${station}FilterList_1in10_${year}.txt
-
 counter=0
 while read line
 do
-
 	echo $line
-	qsub -v RUNDIR=$RunDir,OUTPUTDIR=$OutputDir,STATION=$station,YEAR=$year,FILE=$line,PROCDIR=$ProcDir -N 'join_A'$station'_'$year'_'$counter run.sh
-	counter=$((counter+1))
 
+	if [ $simulation == '1' ] #is simulation
+	then
+		qsub -e $err_out_location -o $err_out_location -v ERRFILE=$ErrFile,RUNDIR=$RunDir,OUTPUTDIR=$OutputDir,STATION=$station,YEAR=$year,FILE=$line,PROCDIR=$ProcDir -N 'join_A'$station'_c'$cibfug'_E'$energy'_'$counter run.sh
+	elif [ $simulation == '0' ] #is not simulation
+	then
+		qsub -e $err_out_location -o $err_out_location -v ERRFILE=$ErrFile,RUNDIR=$RunDir,OUTPUTDIR=$OutputDir,STATION=$station,YEAR=$year,FILE=$line,PROCDIR=$ProcDir -N 'join_A'$station'_'$year'_'$counter run.sh
+	fi	
+	counter=$((counter+1))
 done < $readfile
