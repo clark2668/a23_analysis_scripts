aa2e761f3bf499206f823e6233762a21a015ca01
clark2668 <clark.2668@osu.edu>
2019-04-17 23:40:20 -0400

    revamp reco scripts to better deal with simulation and pass more variables to the qsub

--- osc_scripts/run_diffuse/step4-reco/run_single.sh
+++ osc_scripts/run_diffuse/step4-reco/run_single.sh
@@ -1,10 +1,7 @@
 #/bin/bash
 #PBS -l nodes=1:ppn=1
 #PBS -l mem=4GB
-#PBS -l walltime=48:00:00
 #PBS -A PCON0003
-#PBS -e /fs/scratch/PAS0654/ara/10pct/err_out_logs
-#PBS -o /fs/scratch/PAS0654/ara/10pct/err_out_logs
 
 eval 'source /users/PAS0654/osu0673/A23_analysis_new2/env.sh'
 
