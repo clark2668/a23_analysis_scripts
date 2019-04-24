f5ca8a30ad3fb2b963634ea7e636e933ea183835
clark2668 <clark.2668@osu.edu>
2019-04-24 10:05:49 -0400

    make github happy with submission script and add terminal version

--- osc_scripts/run_diffuse/step5-join_filter_reco/doMerge.sh
+++ osc_scripts/run_diffuse/step5-join_filter_reco/doMerge.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-station="3"
+station="2"
 echo '[ Station: ' $station ' ]'
 export station
 
-year="2016"
+year="2015"
 echo '[ Station: ' $year ' ]'
 export year
 
@@ -47,6 +47,7 @@ export ProcDir
 RunDir="/users/PAS0654/osu0673/A23_analysis_new2/AraRoot"
 export RunDir
 
+eval 'source /users/PAS0654/osu0673/A23_analysis_new2/env.sh'
 counter=0
 while read line
 do
@@ -57,7 +58,14 @@ do
 		qsub -e $err_out_location -o $err_out_location -v ERRFILE=$ErrFile,RUNDIR=$RunDir,OUTPUTDIR=$OutputDir,STATION=$station,YEAR=$year,FILE=$line,PROCDIR=$ProcDir -N 'join_A'$station'_c'${config}'_E'$energy'_'$counter run.sh
 	elif [ $simulation == '0' ] #is not simulation
 	then
-		qsub -e $err_out_location -o $err_out_location -v ERRFILE=$ErrFile,RUNDIR=$RunDir,OUTPUTDIR=$OutputDir,STATION=$station,YEAR=$year,FILE=$line,PROCDIR=$ProcDir -N 'join_A'$station'_'$year'_'$counter run.sh
+		# qsub -e $err_out_location -o $err_out_location -v ERRFILE=$ErrFile,RUNDIR=$RunDir,OUTPUTDIR=$OutputDir,STATION=$station,YEAR=$year,FILE=$line,PROCDIR=$ProcDir -N 'join_A'$station'_'$year'_'$counter run.sh
+		cd $RunDir
+		./bin/v2_Join $station $OutputDir $ProcDir $line
+		if [ $? -ne 0 ] #error handle
+		then
+			echo $line >> /fs/scratch/PAS0654/ara/10pct/Joined/problems_A${station}_${year}_join_errors.txt
+		fi
+
 	fi	
 	counter=$((counter+1))
 done < $readfile
--- osc_scripts/run_diffuse/step5-join_filter_reco/run.sh
+++ osc_scripts/run_diffuse/step5-join_filter_reco/run.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 #PBS -l nodes=1:ppn=1
 #PBS -l mem=4GB
-#PBS -l walltime=00:05:00
+#PBS -l walltime=00:02:00
 #PBS -A PAS0654
 
 eval 'source /users/PAS0654/osu0673/A23_analysis_new2/env.sh'
