7058ea6b9cc62a00229043836547a36da5d6f48d
clark2668 <clark.2668@osu.edu>
2019-08-06 16:07:38 -0400

    clearly this is out of date, and needs sync with github

--- osc_scripts/run_diffuse/step4-reco/v2_multi_owens/owens_run.sh
+++ osc_scripts/run_diffuse/step4-reco/v2_multi_owens/owens_run.sh
@@ -3,88 +3,88 @@
 
 cd $LAUNCHDIR
 
-./owens_doThis.sh $SIMULATION $STATION $YEAR $RADBIN $TMPDIR $F1 $P1 &
+./owens_doThis.sh $SIMULATION $STATION $CONFIG $RADBIN $FILTERDIR $TMPDIR $F1 &
 declare pid_1=$!
 declare F_1=$F1
-./owens_doThis.sh $SIMULATION $STATION $YEAR $RADBIN $TMPDIR $F2 $P2 &
+./owens_doThis.sh $SIMULATION $STATION $CONFIG $RADBIN $FILTERDIR $TMPDIR $F2 &
 declare pid_2=$!
 declare F_2=$F2
-./owens_doThis.sh $SIMULATION $STATION $YEAR $RADBIN $TMPDIR $F3 $P3 &
+./owens_doThis.sh $SIMULATION $STATION $CONFIG $RADBIN $FILTERDIR $TMPDIR $F3 &
 declare pid_3=$!
 declare F_3=$F3
-./owens_doThis.sh $SIMULATION $STATION $YEAR $RADBIN $TMPDIR $F4 $P4 &
+./owens_doThis.sh $SIMULATION $STATION $CONFIG $RADBIN $FILTERDIR $TMPDIR $F4 &
 declare pid_4=$!
 declare F_4=$F4
-./owens_doThis.sh $SIMULATION $STATION $YEAR $RADBIN $TMPDIR $F5 $P5 &
+./owens_doThis.sh $SIMULATION $STATION $CONFIG $RADBIN $FILTERDIR $TMPDIR $F5 &
 declare pid_5=$!
 declare F_5=$F5
-./owens_doThis.sh $SIMULATION $STATION $YEAR $RADBIN $TMPDIR $F6 $P6 &
+./owens_doThis.sh $SIMULATION $STATION $CONFIG $RADBIN $FILTERDIR $TMPDIR $F6 &
 declare pid_6=$!
 declare F_6=$F6
-./owens_doThis.sh $SIMULATION $STATION $YEAR $RADBIN $TMPDIR $F7 $P7 &
+./owens_doThis.sh $SIMULATION $STATION $CONFIG $RADBIN $FILTERDIR $TMPDIR $F7 &
 declare pid_7=$!
 declare F_7=$F7
-./owens_doThis.sh $SIMULATION $STATION $YEAR $RADBIN $TMPDIR $F8 $P8 &
+./owens_doThis.sh $SIMULATION $STATION $CONFIG $RADBIN $FILTERDIR $TMPDIR $F8 &
 declare pid_8=$!
 declare F_8=$F8
-./owens_doThis.sh $SIMULATION $STATION $YEAR $RADBIN $TMPDIR $F9 $P9 &
+./owens_doThis.sh $SIMULATION $STATION $CONFIG $RADBIN $FILTERDIR $TMPDIR $F9 &
 declare pid_9=$!
 declare F_9=$F9
-./owens_doThis.sh $SIMULATION $STATION $YEAR $RADBIN $TMPDIR $F10 $P10 &
+./owens_doThis.sh $SIMULATION $STATION $CONFIG $RADBIN $FILTERDIR $TMPDIR $F10 &
 declare pid_10=$!
 declare F_10=$F10
-./owens_doThis.sh $SIMULATION $STATION $YEAR $RADBIN $TMPDIR $F11 $P11 &
+./owens_doThis.sh $SIMULATION $STATION $CONFIG $RADBIN $FILTERDIR $TMPDIR $F11 &
 declare pid_11=$!
 declare F_11=$F11
-./owens_doThis.sh $SIMULATION $STATION $YEAR $RADBIN $TMPDIR $F12 $P12 &
+./owens_doThis.sh $SIMULATION $STATION $CONFIG $RADBIN $FILTERDIR $TMPDIR $F12 &
 declare pid_12=$!
 declare F_12=$F12
-./owens_doThis.sh $SIMULATION $STATION $YEAR $RADBIN $TMPDIR $F13 $P13 &
+./owens_doThis.sh $SIMULATION $STATION $CONFIG $RADBIN $FILTERDIR $TMPDIR $F13 &
 declare pid_13=$!
 declare F_13=$F13
-./owens_doThis.sh $SIMULATION $STATION $YEAR $RADBIN $TMPDIR $F14 $P14 &
+./owens_doThis.sh $SIMULATION $STATION $CONFIG $RADBIN $FILTERDIR $TMPDIR $F14 &
 declare pid_14=$!
 declare F_14=$F14
-./owens_doThis.sh $SIMULATION $STATION $YEAR $RADBIN $TMPDIR $F15 $P15 &
+./owens_doThis.sh $SIMULATION $STATION $CONFIG $RADBIN $FILTERDIR $TMPDIR $F15 &
 declare pid_15=$!
 declare F_15=$F15
-./owens_doThis.sh $SIMULATION $STATION $YEAR $RADBIN $TMPDIR $F16 $P16 &
+./owens_doThis.sh $SIMULATION $STATION $CONFIG $RADBIN $FILTERDIR $TMPDIR $F16 &
 declare pid_16=$!
 declare F_16=$F16
-./owens_doThis.sh $SIMULATION $STATION $YEAR $RADBIN $TMPDIR $F17 $P17 &
+./owens_doThis.sh $SIMULATION $STATION $CONFIG $RADBIN $FILTERDIR $TMPDIR $F17 &
 declare pid_17=$!
 declare F_17=$F17
-./owens_doThis.sh $SIMULATION $STATION $YEAR $RADBIN $TMPDIR $F18 $P18 &
+./owens_doThis.sh $SIMULATION $STATION $CONFIG $RADBIN $FILTERDIR $TMPDIR $F18 &
 declare pid_18=$!
 declare F_18=$F18
-./owens_doThis.sh $SIMULATION $STATION $YEAR $RADBIN $TMPDIR $F19 $P19 &
+./owens_doThis.sh $SIMULATION $STATION $CONFIG $RADBIN $FILTERDIR $TMPDIR $F19 &
 declare pid_19=$!
 declare F_19=$F19
-./owens_doThis.sh $SIMULATION $STATION $YEAR $RADBIN $TMPDIR $F20 $P20 &
+./owens_doThis.sh $SIMULATION $STATION $CONFIG $RADBIN $FILTERDIR $TMPDIR $F20 &
 declare pid_20=$!
 declare F_20=$F20
-./owens_doThis.sh $SIMULATION $STATION $YEAR $RADBIN $TMPDIR $F21 $P21 &
+./owens_doThis.sh $SIMULATION $STATION $CONFIG $RADBIN $FILTERDIR $TMPDIR $F21 &
 declare pid_21=$!
 declare F_21=$F21
-./owens_doThis.sh $SIMULATION $STATION $YEAR $RADBIN $TMPDIR $F22 $P22 &
+./owens_doThis.sh $SIMULATION $STATION $CONFIG $RADBIN $FILTERDIR $TMPDIR $F22 &
 declare pid_22=$!
 declare F_22=$F22
-./owens_doThis.sh $SIMULATION $STATION $YEAR $RADBIN $TMPDIR $F23 $P23 &
+./owens_doThis.sh $SIMULATION $STATION $CONFIG $RADBIN $FILTERDIR $TMPDIR $F23 &
 declare pid_23=$!
 declare F_23=$F23
-./owens_doThis.sh $SIMULATION $STATION $YEAR $RADBIN $TMPDIR $F24 $P24 &
+./owens_doThis.sh $SIMULATION $STATION $CONFIG $RADBIN $FILTERDIR $TMPDIR $F24 &
 declare pid_24=$!
 declare F_24=$F24
-./owens_doThis.sh $SIMULATION $STATION $YEAR $RADBIN $TMPDIR $F25 $P25 &
+./owens_doThis.sh $SIMULATION $STATION $CONFIG $RADBIN $FILTERDIR $TMPDIR $F25 &
 declare pid_25=$!
 declare F_25=$F25
-./owens_doThis.sh $SIMULATION $STATION $YEAR $RADBIN $TMPDIR $F26 $P26 &
+./owens_doThis.sh $SIMULATION $STATION $CONFIG $RADBIN $FILTERDIR $TMPDIR $F26 &
 declare pid_26=$!
 declare F_26=$F26
-./owens_doThis.sh $SIMULATION $STATION $YEAR $RADBIN $TMPDIR $F27 $P27 &
+./owens_doThis.sh $SIMULATION $STATION $CONFIG $RADBIN $FILTERDIR $TMPDIR $F27 &
 declare pid_27=$!
 declare F_27=$F27
-./owens_doThis.sh $SIMULATION $STATION $YEAR $RADBIN $TMPDIR $F28 $P28 &
+./owens_doThis.sh $SIMULATION $STATION $CONFIG $RADBIN $FILTERDIR $TMPDIR $F28 &
 declare pid_28=$!
 declare F_28=$F28
 
