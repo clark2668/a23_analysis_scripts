f9257cbca58f9efe6bddb0c93ab480da4436aaf8
clark2668 <clark.2668@osu.edu>
2019-08-07 11:48:30 -0500

    add some scripts for making ped pairs for 100pct wipac

---
+++ wipac_scripts/100pct/step1-make_ped_pairs/makeA23PedFileList.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+#this makes a list of all pedestal files for the station A23 Diffuse analysis, years 2013-2016
+#results are stored in pedestalValues_station_year.txt folders
+
+#run like "bash makeA23PedFileList.sh station year"
+#like "bash makeA23PedFileList.sh 2 2013"
+
+station=$1
+year=$2
+
+echo "[ Station: " $station " ] "
+echo "[ Year: " $year " ] "
+
+#we have to differentiate over years and stations because the location changed
+
+if [ $year -eq 2013 ]
+then
+	if [ $station -eq 2 ]
+	then
+		find /data/user/brianclark/A23Diffuse/100pct/peds/A2_2013_peds/run*/*pedestalValues* > pedestalValues_A2_2013.txt
+	fi
+elif [ $year -eq 2014 ]
+then
+	if [ $station -eq 2 ]
+	then
+		find /data/wipac/ARA/2014/calibration/pedestals/ARA02/*pedestalValues* > pedestalValues_A2_2014.txt
+	elif [ $station -eq 3 ]
+	then
+		find /data/wipac/ARA/2014/calibration/pedestals/ARA03/*pedestalValues* > pedestalValues_A3_2014.txt 
+	fi
+elif [ $year -eq 2015 ]
+then
+	if [ $station -eq 2 ]
+	then
+		find /data/wipac/ARA/2015/calibration/pedestals/ARA02/*pedestalValues* > pedestalValues_A2_2015.txt
+	elif [ $station -eq 3 ]
+	then
+		find /data/wipac/ARA/2015/calibration/pedestals/ARA03/*pedestalValues* > pedestalValues_A3_2015.txt 
+	fi
+elif [ $year -eq 2016 ]
+then
+	if [ $station -eq 2 ]
+	then
+		find /data/wipac/ARA/2016/calibration/pedestals/ARA02/*pedestalValues* > pedestalValues_A2_2016.txt
+	elif [ $station -eq 3 ]
+	then
+		find /data/wipac/ARA/2016/calibration/pedestals/ARA03/*pedestalValues* > pedestalValues_A3_2016.txt
+	fi
+fi
