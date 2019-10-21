3885db26eade248cb5b66f13ac2bc799a6bd2238
clark2668 <clark.2668@osu.edu>
2019-10-21 18:26:12 -0400

    move all run references in the 10pct processing code to project space intead of scratch space, but keep backups for archival purposes

--- osc_scripts/run_diffuse/step1-make_ped_pairs/copyPeds.sh
+++ osc_scripts/run_diffuse/step1-make_ped_pairs/copyPeds.sh
@@ -10,7 +10,8 @@ while read line; do
 		#comment out the following line if you don't want to strip leading zeros
 		runNo=$(echo $runNoWithZero | sed 's/^0*//') #remove leading zeros	
 		
-		new_save='/fs/scratch/PAS0654/ara/peds/run_specific_peds/A3/all_peds/event'$runNo'_specificPeds.dat' #make a new filename
+#		new_save='/fs/scratch/PAS0654/ara/peds/run_specific_peds/A3/all_peds/event'$runNo'_specificPeds.dat' #make a new filename
+		new_save='/fs/project/PAS0654/ARA_DATA/A23/peds/run_specific_peds/A2/all_peds/event'$runNo'_specificPeds.dat'
 		echo 'new save name: ' $new_save
 		pedName=${line#* }
 		echo 'ped name: ' $pedName
@@ -18,4 +19,4 @@ while read line; do
 	else
 		echo "Pedestal not specified; nothing to be done."
 	fi
-done <A3_2016_File_Ped_Pairs.txt
+done <A2_2016_File_Ped_Pairs.txt
