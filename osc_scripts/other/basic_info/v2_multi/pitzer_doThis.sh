ac3c83238808aabf74d60f9b2b46e3bcaeb4bba4
clark2668 <clark.2668@osu.edu>
2019-11-10 15:48:42 -0500

    make git happy for basic info processing scripts, and add A3 data and sim file lists

--- osc_scripts/other/basic_info/v2_multi/pitzer_doThis.sh
+++ osc_scripts/other/basic_info/v2_multi/pitzer_doThis.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 eval 'source /users/PAS0654/osu0673/A23_analysis_new2/env.sh'
 cd $RUNDIR
-./bin/BasicEventInfo $1 $2 $3
+#./bin/BasicEventInfo $1 $2 $3
+./bin/BasicEventInfo_Sim $1 $2 $3
